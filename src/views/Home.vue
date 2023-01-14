b4af2b12415579f2e5431b7b72a48d5b5caf80c7
Peach <2990221345@qq.com>
2023-01-14 20:54:48 +0800

    updata

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,11 @@
 <template>
-  <div class="home">welcome to Peach Blog</div>
+  <div class="home">
+    <div>welcome to Peach Blog</div>
+    <div>我是一名前端开发工程师</div>
+  </div>
 </template>
 
 <script>
-// @ is an alias to /src
-
 export default {
   name: "Home",
   components: {},
